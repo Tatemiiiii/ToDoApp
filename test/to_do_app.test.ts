14326d4d752b4ce4c57e3317c9e4916267fa0b41
Tatemiiiii <yosvf2@gmail.com>
2023-01-22 10:45:05 +0000

    #5 AWS CDKプロジェクトの初期化

---
+++ test/to_do_app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ToDoApp from '../lib/to_do_app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/to_do_app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ToDoApp.ToDoAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
