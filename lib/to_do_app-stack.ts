dc33a915e2775b8d999d4bf64b2dc767e8c27a57
Tatemiiiii <yosvf2@gmail.com>
2023-01-29 11:06:42 +0000

    #5 S3バケット作成

--- lib/to_do_app-stack.ts
+++ lib/to_do_app-stack.ts
@@ -1,11 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
 export class ToDoAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    new s3.Bucket(this, 'ToDoApp', {
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL
+    })
     // The code that defines your stack goes here
 
     // example resource
