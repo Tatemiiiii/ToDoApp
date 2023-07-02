1cb897e7c14c40e11596f4cb3c673f4f858c981e
Tatemiiiii <yosvf2@gmail.com>
2023-07-02 10:57:54 +0000

    #16 lambdaのテスト作成

--- lib/to_do_app-stack.ts
+++ lib/to_do_app-stack.ts
@@ -4,10 +4,30 @@ import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import path from 'path';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { RetentionDays } from 'aws-cdk-lib/aws-logs';
 
 export class ToDoAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    //Dynamodb作成
+    const table = new dynamodb.Table(this, 'Todo', {
+      partitionKey: { name: 'status', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'id', type: dynamodb.AttributeType.STRING }
+    });
+
+    //Lambda作成
+    const func = new NodejsFunction(this, 'DynamodbTest', {
+      entry: path.join(__dirname, './lambda/Dynamo_test/index.ts'),
+      handler: 'handler',
+      runtime: Runtime.NODEJS_18_X, //indexを実行するための実行エンジンを指定する
+      timeout: cdk.Duration.minutes(1),
+      logRetention: RetentionDays.THREE_MONTHS
+    });
 
     //
     const sourceBucket = new s3.Bucket(this, 'ToDoApp', {
